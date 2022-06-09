e1b251ffad83699eba419b37d500797a4997b9c0
Robert Santos <robertdosantos09@gmail.com>
2022-06-09 20:10:25 -0300

    Resolução dos exercicios do bloco 7.1 questões 1 e 2.

---
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/dia1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,30 @@
+// Questão 1
+
+const testingScope = (escopo) => {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
+    } 
+    else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
+  console.log('------------------------------------------------------------------------');
+
+// Questão 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const ordemNumbers = (array) => {
+    let arrDeOrdem = array.sorte((x, z) => x - z);
+    
+    return arrDeOrdem;
+}
+
+console.log(ordemNumbers(oddsAndEvens));
+console.log('------------------------------------------------------------------------');
+
+// Questão 3
