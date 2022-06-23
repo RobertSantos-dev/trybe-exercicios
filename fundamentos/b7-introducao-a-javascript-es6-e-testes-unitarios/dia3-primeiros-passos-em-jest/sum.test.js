55cc20de04c99fc8d236918903c1335a607b6a4f
Robert Santos <robertdosantos09@gmail.com>
2022-06-23 19:44:01 -0300

    Resolução dos exercicios do bloco 7.3 questão 1

---
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,18 @@
+const sum = require('./sum.js');
+
+describe('Testes na função sum, que faz a soma de paramentros', () => {
+    test('1° teste', () => {
+        expect(sum(4, 5)).toEqual(9);
+    });
+    test('2° teste', () => {
+        expect(sum(0, 0)).toEqual(0);
+    });
+    test('3° teste', () => {
+        expect(() => {sum(4, '5')}).toThrow();
+    });
+    test('4° teste', () => {
+        expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers'));
+      //expect(() => {sum(4, '5')}).toThrowError('parameters must be numbers');
+    });
+})
+//
