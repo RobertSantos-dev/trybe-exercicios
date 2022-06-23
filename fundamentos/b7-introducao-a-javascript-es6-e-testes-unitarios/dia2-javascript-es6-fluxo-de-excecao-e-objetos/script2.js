895ea8f9603494f931f814348545fd69edac51b0
Robert Santos <robertdosantos09@gmail.com>
2022-06-23 00:14:48 -0300

    Resolução dos exercicios do bloco 7.2 parte 3 questão 5 e alguns ajustes

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/dia2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/dia2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
@@ -20,5 +20,20 @@ const lesson3 = {
 
 const addObj = (obj, ch, val) => obj[ch] = val;
 const listObj = (obj) => Object.keys(obj);
-const tamObj = (obj) => Object.entries(obj);
+const tamObj = (obj) => Object.entries(obj).length;
 const valObj = (obj) => Object.values(obj);
+
+addObj(lesson2,'turno','noite');
+
+console.log(lesson2);
+console.log("-----------------------");
+console.log(listObj(lesson3));
+console.log("-----------------------");
+console.log(tamObj(lesson1));
+console.log("-----------------------");
+console.log(valObj(lesson2));
+
+const allLessons = {};
+Object.assign(allLessons, {lesson1, lesson2, lesson3});
+
+console.log(allLessons);
