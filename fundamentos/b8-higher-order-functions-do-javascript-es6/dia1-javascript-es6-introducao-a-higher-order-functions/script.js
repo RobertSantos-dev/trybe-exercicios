7d3140b4055ed297372c65278b9eb2769c174a9c
Robert Santos <robertdosantos09@gmail.com>
2022-06-23 22:22:10 -0300

    Resolução dos exercicios do bloco 8.1 questão 3

--- fundamentos/b8-higher-order-functions-do-javascript-es6/dia1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/b8-higher-order-functions-do-javascript-es6/dia1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -16,6 +16,7 @@ newEmployees('Luiza Drumond');
 newEmployees('Carla Paiva');
 console.log(employees);
 console.log('----------------------------------------------------');
+
 //Questão 2
 const sorteio = (n) => {
     let ns = Math.round(Math.random() * 5);
@@ -25,5 +26,24 @@ const sorteio = (n) => {
 
     return res;
 }
-
 console.log(sorteio(3));
+console.log('----------------------------------------------------');
+
+//Questão 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
+const ver = (arrGab, arrStu) => {
+    let res = [0,0];
+    for (let i in arrGab) {
+        if (arrGab[i] === arrStu[i] && arrStu[i] !== 'N.A') { res[0] += 1; }
+        else if (arrGab[i] !== arrStu[i] && arrStu[i] !== 'N.A') { res[1] += 1; }
+    }
+    return `Acertos: ${res[0]} | Erros: ${res[1]}`;
+}
+const avaliador = (arr1, arr2, verificador) => {
+    return verificador(arr1, arr2);
+}
+
+console.log(avaliador(RIGHT_ANSWERS, STUDENT_ANSWERS, ver));
