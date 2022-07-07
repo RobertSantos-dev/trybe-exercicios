319cbca65a7655bcafee71d528b11a11a02f7a88
Robert Santos <robertdosantos09@gmail.com>
2022-07-07 19:04:59 -0300

    Resolução dos exercicios do bloco 9.1 questão 3

--- frontEnd/b9-javascript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercise.js
+++ frontEnd/b9-javascript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercise.js
@@ -38,3 +38,12 @@ console.log(planetDistanceFromSun(mars)); // A
 setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
 setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
 // A, C e depois B
+
+setTimeout(() => console.log(' =============================== '), 3500);
+
+// Questão 3
+const getPlanet = (planet) => {
+  console.log("Returned planet: ", planet);
+};
+
+setTimeout(() => getPlanet(mars), 4000); // imprime Marte depois de 4 segundos
