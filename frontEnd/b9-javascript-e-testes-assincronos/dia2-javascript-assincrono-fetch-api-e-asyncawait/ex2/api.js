99c9e274a7ab064f03c5df43d8d169884e7bea2e
Robert Santos <robertdosantos09@gmail.com>
2022-07-08 21:44:11 -0300

    Resolução dos exercicios do bloco 9.2 questão 2.4

--- frontEnd/b9-javascript-e-testes-assincronos/dia2-javascript-assincrono-fetch-api-e-asyncawait/ex2/api.js
+++ frontEnd/b9-javascript-e-testes-assincronos/dia2-javascript-assincrono-fetch-api-e-asyncawait/ex2/api.js
@@ -1,5 +1,8 @@
 const endUrl = 'https://api.coincap.io/v2/assets';
 const listCoins = document.querySelector('#list-coins');
+const btnTop10 = document.querySelector('#btn-top10');
+
+let cont = 0;
 
 const listItem = (element, str) => {
     let e = document.createElement(element);
@@ -7,16 +10,39 @@ const listItem = (element, str) => {
     return e;
 };
 
+const resetList1 = (arr) => listCoins.querySelectorAll('li').forEach((e, i) => i < 10 ? arr.push(e) : '');
+const resetList2 = () => listCoins.querySelectorAll('li').forEach(e => e.remove());
+
+const top10 = () => {
+    let res = []
+    if (cont === 0) {
+        resetList1(res);
+        resetList2();
+        res.map((e) => listCoins.appendChild(listItem('li', e.innerHTML)));
+        btnTop10.innerHTML = 'Mostrar todos';
+        cont += 1;
+    } else {
+        cont = 0;
+        resetList2();
+        moedas();
+        btnTop10.innerHTML = 'Top 10';
+    }
+};
+
 const moedas = () => {
     let item;
     fetch(endUrl)
     .then((res) => res.json())
     .then((obj) => {
-        obj.data.map((e) => {
-            item = listItem('li',`${e.name} (${e.symbol}): ${e.priceUsd}`);
+        obj.data.map((e, i) => {
+            item = listItem('li',`${i + 1} - ${e.name} (${e.symbol}): ${e.priceUsd}`);
             listCoins.appendChild(item);
-        });
+        });   
+        if (listCoins.children.length > 10) {
+            btnTop10.disabled = false;
+            btnTop10.addEventListener('click', top10);
+        }
     });
 };
 
-moedas();
+window.onload = moedas();
